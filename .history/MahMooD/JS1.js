719f68a945af52714dd032e82046b21eb83dbb65
MahMooD-700 <elmahmudix@gmail.com>
2024-12-12 15:08:56 +0200

    Add files via upload

---
+++ .history/MahMooD/JS1.js
@@ -0,0 +1,12 @@
+var x = 2-20 ,y = 2 , n="MahmooD" ,z="10" ,c=10 , p=
+console.log(x*y)
+console.log(typeof x,n)
+console.log(z==c)  // compare between the values
+console.log(z===c) // compare between the types of data 
+console.log(x+n+c)
+console.log(x-n) // it shows NaN (not a number )
+console.log(n+p) // string + not a defined var
+console.log(y*n)
+console.log(x*c/y+n) // working
+console.log(x*c/y/n)// not working cause we can only connect numbers with string using (+)
+// and the Null we can treat it as a bug and 
