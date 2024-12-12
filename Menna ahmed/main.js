c403338f63bd4040116ffc311c71d93f0b0238e4
Menna Ahmed <menna128492@gmail.com>
2024-12-12 17:48:08 +0200

    Add files via upload

---
+++ Menna ahmed/main.js
@@ -0,0 +1,52 @@
+console.log(typeof 8);
+console.log(typeof false);
+console.log(typeof "menna");
+
+var x=20, y=12;
+var result= x+y;
+console.log(result);
+var result=x-y;
+console.log(result);
+var result=x/y;
+console.log(result);
+var result=x*y;
+console.log(result);
+var result=x**y;
+console.log(result);
+var result=(x+y)/y;
+console.log(result);
+
+var x=5;
+x+=1;
+console.log(x);
+
+var x=4 , y=8;
+var result= x>y;
+console.log(result);
+var result= x<=y;
+console.log(result);
+y=4;
+result= x==y;
+console.log(result);
+
+var firstName = "Menna ";
+var lastName = "Ahmed";
+console.log(firstName+lastName);
+
+var x1=4;
+var x2="5";
+console.log(x1+x2);
+
+var x1="4";
+var x2="5";
+console.log(x1*x2);
+
+var x1="4";
+var x2="5M";
+console.log(x1-x2);
+
+var bool="10"==10;
+console.log(bool);
+
+
+
