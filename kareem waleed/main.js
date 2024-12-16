2c6d8d4d86c720a59976cfbac581fefe7c9af63c
kareem walead <kareemwalead91@gmail.com>
2024-12-16 19:17:36 +0200

    Add files via upload

---
+++ kareem waleed/main.js
@@ -0,0 +1,69 @@
+
+  var y=2;
+  // console.log(typeof aya)
+  // data types : number , string , boolean , null , undefined , typeof
+  // variables =>(let , const , var )------------
+  // Math.pow(2,3)  الاس**
+  // % باقي قسمة
+  //   ==  سؤال هل يساوي ام لا 
+  var x = 2;
+  var y = 2;
+  var z = 3;
+  var result = (z * x)/ y ;
+  console.log(result);
+  var x = 3; 
+  x = x + 1 ;
+  x **=2;
+  console.log(x);
+//   ----------.
+var x = 5 , y = 5;
+var result= x == y ;
+console.log(result);
+y = 3;
+result = x == y;
+console.log(result);
+
+// let x = 5;
+// const x= 5;
+// string-------------
+var firstName ="aya";
+var lastName="khaled is a developer"
+var lastName2="is 21 "
+var userName = firstName + lastName;
+console.log(firstName , lastName);
+console.log(firstName + lastName);  // بدون space مسافات
+console.log(userName);
+console.log( lastName2);
+// document.getElementById
+
+
+
+var x1 ="10";
+var x2 ="11A";
+console.log(x1 + x2);
+console.log(x1 * x2);
+console.log(x1 / x2);
+console.log(x1 - x2);
+console.log(x1 ** x2);
+// NaN------Not a Number- - - -  --->
+var o;
+console.log(o);
+// undefined----->
+let res ="Hello" / 2;
+console.log(res)
+// NaN----------
+var bool = 4 == 5;
+console.log(bool);
+// false--------
+var bool = "5" == 5;
+console.log(bool);
+// true------
+var bool = "5" === 5;
+console.log(bool);
+// === هل يساوي في القيمة والنوع؟
+// false-----------
+// لا يسمح باستخدام كلمات محجوزة في تسمية المتغير
+// لا تستخدم name في طباعتها
+// لا يسمح بأستخدام العلامات زي#$%^&* في بداية اسم المتغير
+var name1 = null;
+console.log(name1)
